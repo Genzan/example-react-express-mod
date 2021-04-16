bd5525ad930f63b98c7836c7dfb0a9af8af801c5
Alan <Alan.Escudero.Caporal@gmail.com>
2021-04-16 11:11:45 -0500

    Initial Push

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -1,7 +1,45 @@
 import { useEffect, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { UserContext } from '../lib/UserContext';
+import { magic } from '../lib/magic';
+import Web3 from 'web3';
 import Loading from './loading';
+
+const web3 = new Web3(magic.rpcProvider);
+
+const getData = async () => {
+  //const network = await web3.eth.net.getNetworkType();
+  //console.log("Network",network);
+  const userAddress = (await web3.eth.getAccounts())[0];
+  console.log("userAddress",userAddress);
+  /*const userBalance = web3.utils.fromWei(
+    await web3.eth.getBalance(userAddress) // Balance is in wei
+  );
+  console.log("userBalance",userBalance);*/
+  updateUser();
+}
+
+const updateUser = async () => {
+  const contractAddressRinkeby = "0xe652135baeF1b5b22E662e8120A50aE947119092"; //Rinkeby
+  const contractAddressCIIAN = "0x6983DEd5D97AEF64FA76682610CC9104039Bd528"; //CIIAN
+  const contractABI = '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"_user","type":"address"},{"indexed":false,"internalType":"string","name":"_perfilHash","type":"string"}],"name":"PerfilAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"_user","type":"address"},{"indexed":false,"internalType":"string","name":"_perfilHash","type":"string"}],"name":"PerfilUpdated","type":"event"},{"inputs":[{"internalType":"string","name":"_perfilHash","type":"string"}],"name":"updateUser","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"contractOwner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_user","type":"address"}],"name":"getUserData","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_user","type":"address"}],"name":"isUserActive","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}]';
+  const fromAddress = (await web3.eth.getAccounts())[0];
+  const newMessage = "Indra";
+  const contract = new web3.eth.Contract(
+    JSON.parse(contractABI),
+    contractAddressCIIAN
+  );
+  try{
+  await contract.methods
+    .updateUser(newMessage)
+    .send({ from: fromAddress })
+    .on('receipt', function(receiptTran){
+      console.log("Completed:", receiptTran);
+    });
+  } catch {
+    console.log("Send");
+  }
+}
 
 const Profile = () => {
   const history = useHistory();
@@ -11,6 +49,8 @@ const Profile = () => {
   useEffect(() => {
     user && !user.loading && !user.issuer && history.push('/login');
   }, [user, history]);
+
+  getData();
 
   return (
     <>
@@ -23,6 +63,9 @@ const Profile = () => {
             <div className='profile-info'>{user.email}</div>
 
             <div className='label'>User Id</div>
+            <div className='profile-info'>{user.issuer}</div>
+
+            <div className='label'>Address</div>
             <div className='profile-info'>{user.issuer}</div>
           </>
         )
--- client/src/lib/magic.js
+++ client/src/lib/magic.js
@@ -1,6 +1,13 @@
 import { Magic } from 'magic-sdk';
 import { OAuthExtension } from '@magic-ext/oauth';
 
+
+const customNodeOptions = {
+  rpcUrl: 'http://189.197.77.190:8545',
+  chainId: 10
+}
+
 export const magic = new Magic(process.env.REACT_APP_MAGIC_PUBLISHABLE_KEY, {
   extensions: [new OAuthExtension()],
+  network: customNodeOptions,
 });
